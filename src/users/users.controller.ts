99a597d3d1dff2f574190e32bdd3f335468bd83d
Gibeom Choi <kcdevdes@gmail.com>
2024-08-01 20:22:57 +0900

    added google with JWT

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,23 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Req,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
+import { AccessTokenGuard } from 'src/auth/guards/bearer-token.guard';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  @UseGuards(AccessTokenGuard)
+  getUser(@Req() req) {
+    if (!req) {
+      throw new UnauthorizedException('No User Found in Request');
+    }
+    return this.usersService.findUserByEmail(req.user.email);
+  }
 }
